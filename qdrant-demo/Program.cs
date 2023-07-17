63da3b5a66b4cd6337d1022bdd70c7868653b962
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2023-07-17 01:18:17 +0000

    added more Memory examples

--- qdrant-demo/Program.cs
+++ qdrant-demo/Program.cs
@@ -1,33 +1,22 @@
 ﻿using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.Configuration.UserSecrets;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Memory;
 using Microsoft.SemanticKernel.Connectors.Memory.Qdrant;
-//using Microsoft.SemanticKernel.KernelExtensions;
-//using System.IO;
-//using Microsoft.SemanticKernel.Configuration;
-//using Microsoft.SemanticKernel.SemanticFunctions;
-//using Microsoft.SemanticKernel.CoreSkills;
-
 
 
 var config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", true, true)   
+    .AddJsonFile("appsettings.json", true, true)
     .AddUserSecrets<Program>()
     .Build();
 
 string? myAzureOpenAIEmbedDeployment = config["AzureOpenAIEmbedDeployment"];
 string? myAzureOpenAIDeployment = config["AzureOpenAIDeployment"];
 string? myAOAIEndpoint = config["AzureOpenAIEndpoint"];
-string? myAOAIKey = config["AzureOpenAIKey"];    
+string? myAOAIKey = config["AzureOpenAIKey"];
 string? myQdrantEndpoint = config["QdrantEndpoint"];
 
 Console.WriteLine(myAOAIEndpoint);
 Console.WriteLine(myAOAIKey);
-
-// var kernel = Kernel.Builder.Build();
-// kernel.Config.AddAzureTextCompletionService(myAzureOpenAIDeployment!, myAOAIEndpoint!, myAOAIKey!);  // 0.13.442.1-preview.
-// kernel.Config.AddAzureTextEmbeddingGenerationService(myAzureOpenAIDeployment!, myAOAIEndpoint!, myAOAIKey!);  // 0.13.442.1-preview.
 
 //string MemoryCollectionName = "qdrant-test";
 int qdrantVectorSize = 1536;
@@ -37,57 +26,87 @@ QdrantMemoryStore memoryStore = new(myQdrantEndpoint!, qdrantVectorSize);
 IKernel kernel = Kernel.Builder
             // .WithLogger(ConsoleLogger.Log)
             .WithAzureTextCompletionService(myAzureOpenAIDeployment!, myAOAIEndpoint!, myAOAIKey!)
-            .WithAzureTextEmbeddingGenerationService(myAzureOpenAIEmbedDeployment!, myAOAIEndpoint!, myAOAIKey!)            
+            .WithAzureTextEmbeddingGenerationService(myAzureOpenAIEmbedDeployment!, myAOAIEndpoint!, myAOAIKey!)
             .WithMemoryStorage(memoryStore)
             //.WithMemoryStorage(new VolatileMemoryStore())
             //.WithQdrantMemoryStore(Env.Var("QDRANT_ENDPOINT"), 1536) // This method offers an alternative approach to registering Qdrant memory store.
             .Build();
 
-
-// var kernel =  Microsoft.SemanticKernel.Kernel.Builder
-// .Configure(c =>
-// {    
-//     c.AddTextCompletionService()
-//     c.AddAzureTextCompletionService(myAzureOpenAIDeployment!,myAOAIEndpoint!,myAOAIKey!);        
-//     c.AddAzureTextEmbeddingGenerationService(myAzureOpenAIDeployment!,myAOAIEndpoint!,myAOAIKey!);
-    
-// })
-// .WithMemoryStorage(new VolatileMemoryStore())
-// .Build();
-
-
 const string memoryCollectionName = "Facts About Me";
 
-await kernel.Memory.SaveInformationAsync(memoryCollectionName, id: "LinkedIn Bio", 
+var myBio = await kernel.Memory.SaveInformationAsync(memoryCollectionName, id: "LinkedIn Bio",
     text: "I currently work in the hotel industry at the front desk. I won the best team player award.");
 
-await kernel.Memory.SaveInformationAsync(memoryCollectionName, id: "LinkedIn History", 
+var myHistory = await kernel.Memory.SaveInformationAsync(memoryCollectionName, id: "LinkedIn History",
     text: "I have worked as a tourist operator for 8 years. I have also worked as a banking associate for 3 years.");
 
-await kernel.Memory.SaveInformationAsync(memoryCollectionName, id: "Recent Facebook Post", 
+var myRecentFB = await kernel.Memory.SaveInformationAsync(memoryCollectionName, id: "Recent Facebook Post",
     text: "My new dog Trixie is the cutest thing you've ever seen. She's just 2 years old.");
-    
-await kernel.Memory.SaveInformationAsync(memoryCollectionName, id: "Old Facebook Post", 
+
+var myOldFB = await kernel.Memory.SaveInformationAsync(memoryCollectionName, id: "Old Facebook Post",
     text: "Can you believe the size of the trees in Yellowstone? They're huge! I'm so committed to forestry concerns.");
 
 Console.WriteLine("Four GIGANTIC vectors were generated just now from those 4 pieces of text above.");
 
-Console.WriteLine("Press enter to continue...");
-Console.ReadLine();
+// loop thru all the memories in the collection
+
+Console.WriteLine("== Printing Collections in DB ==");
+var printColl = memoryStore.GetCollectionsAsync();
+await foreach (var collection in printColl)
+{
+    Console.WriteLine(collection);
+}
+
+Console.WriteLine("== Retrieving Memories Through the Kernel ==");
+MemoryQueryResult? lookup = await kernel.Memory.GetAsync(memoryCollectionName, "LinkedIn Bio");
+Console.WriteLine(lookup != null ? lookup.Metadata.Text : "ERROR: memory not found");
+
+Console.WriteLine("== Retrieving Memories Directly From the Store ==");
+var memory1 = await memoryStore.GetWithPointIdAsync(memoryCollectionName, myOldFB);
+Console.WriteLine(memory1 != null ? memory1.Metadata.Text : "ERROR: memory not found");
+
+Console.WriteLine("== Similarity Searching Memories: My favorite work is ==");
+var searchResults = kernel.Memory.SearchAsync(memoryCollectionName, "My work information", limit: 3, minRelevanceScore: 0.8);
+
+var relatedMemory = "I know nothing.";
+var counter = 0;
+
+await foreach (var item in searchResults)
+{
+    if (counter == 0) { relatedMemory = item.Metadata.Text; }
+    Console.WriteLine(item.Metadata.Text + " : " + item.Relevance);        
+}
+
+var myFunction = kernel.CreateSemanticFunction(@"
+{{$input}}
+Tell me about me and my work history in less than 70 characters.
+", maxTokens: 100, temperature: 0.1, topP: .1);
+
+var result = await myFunction.InvokeAsync(relatedMemory);
+
+Console.WriteLine("Response from AI: " +result);
 
 
-var prompt = @"{{$input}}
+//Add test collection and delete it
+const string memoryCollectionDelete = "Memory to Delete";
 
-One line TLDR with the fewest words.";
-
-var summarize = kernel.CreateSemanticFunction(prompt);
-
-string textBrady = @"
-Tom Brady won a super bowl in the first year he played in the NFL.
-Tom Brady won the super bowl 9 times in his 20 year career.
-He retired after winning a super bowl with the Tampa Bay Bucs.";
+var delMoon = await kernel.Memory.SaveInformationAsync(memoryCollectionDelete, id: "Moon distance",
+    text: "The average distance is 238,855 miles (384,400 kilometers)");
+var delParis = await kernel.Memory.SaveInformationAsync(memoryCollectionDelete, id: "Paris NYC History",
+    text: "The distance between Paris and New York City is approximately 3,631 miles");
 
 
-Console.WriteLine(await summarize.InvokeAsync(textBrady));
+Console.WriteLine("== Printing Collections in DB ==");
+await foreach (var collection in printColl)
+{
+    Console.WriteLine(collection);
+}
 
+Console.WriteLine("== Removing Collection {0} ==", memoryCollectionDelete);
+await memoryStore.DeleteCollectionAsync(memoryCollectionDelete);
 
+Console.WriteLine("== Printing Collections in DB ==");
+await foreach (var collection in printColl)
+{
+    Console.WriteLine(collection);
+}
